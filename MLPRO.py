581ee99a396bf65f00da9d6da82340463e8ae0c4
akankshawankhade <54704735+akankshawankhade@users.noreply.github.com>
2019-09-19 10:31:13 +0530

    Add files via upload

---
+++ MLPRO.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Sep 15 20:34:36 2019
+
+@author: Lenovo
+"""
+
+import webbrowser
+url="cars"
+webbrowser.open_new("https://www.google.com/search?q=cars&rlz=1C1CHZL_enIN822IN822&oq="+url+"&aqs=chrome..69i57.579j0j8&sourceid=chrome&ie=UTF-8")
+
+url="bridges"
+webbrowser.open_new("https://www.google.com/search?rlz=1C1CHZL_enIN822IN822&sxsrf=ACYBGNSYzSZn6r3vowoGIG54TiAMCpqz9Q%3A1568818417534&ei=8USCXaqnIJP6rQGn46nYDw&q=bikes&oq=bikes&gs_l=psy-ab.3..0l6j0i10j0l3.12907.15464..16518...1.1..0.218.930.0j4j1....3..0....1..gws-wiz.....10..0i71j35i362i39j35i39j0i67j0i131.bTCbKQdyjEM&ved=0ahUKEwjq4_mD0NrkAhUTfSsKHadxCvsQ4dUDCAs&uact=5")
+
+url="trains"
+webbrowser.open_new("https://www.google.com/search?q=trains&rlz=1C1CHZL_enIN822IN822&oq=trains&aqs=chrome..69i57j0l5.2970j1j7&sourceid=chrome&ie=UTF-8")
+
+url="gmail"
+webbrowser.open_new("https://www.google.com/search?q=gmail&rlz=1C1CHZL_enIN822IN822&oq=gmail&aqs=chrome..69i57j0l3j69i61l2.2203j1j7&sourceid=chrome&ie=UTF-8")
+
+url="youtube"
+webbrowser.open_new("https://www.youtube.com/")
